148cfe886fe18ada5c0832d19485730b1a0a3242
Kristian K. S. Nielsen <kris9463@edu.sde.dk>
2019-01-21 14:44:08 +0100

    updato

--- src/GUI.cpp
+++ src/GUI.cpp
@@ -45,11 +45,11 @@ void LoadBorder()
     //Top Title
     Label mainTitle(240, 5, "Eternity Squid Studios Controller II", Label::CENTER, Font16, LCD_COLOR_GREEN);
 
-    //Border Lines
-    lcd.DrawRect(0, 25, 479, 225);
-
     //Border Color
     lcd.SetTextColor(LCD_COLOR_WHITE);
+
+    //Border Lines
+    lcd.DrawRect(0, 25, 479, 225);
 }
 
 void LoadUpdateingLabels()
@@ -110,7 +110,10 @@ void LoadMainScreen()
     //Baud for PuTTy 9600 Standard
     PC.baud(9600);
 
+    //Posts the Building number input to serial
     PC.printf("\rBuilding: %d\r\n", buildingNumber);
+
+    //Posts the Room number input to serial
     PC.printf("\rRoom: %d\r\n", roomNumber);
 
     LoadBorder();
@@ -118,12 +121,13 @@ void LoadMainScreen()
     //Start thread for updating labels
     LabelUpdateThread.start(LoadUpdateingLabels);
     
-    //Adds a underline to the counter text
+    //Changes the temperature unit displayed & posts the input to serial
     Button labelBtnTemp(1, 26, 478, 60, "___________", Font16, LCD_COLOR_WHITE, 0xFF003538, 0xFF003538, 0xFF003538);
 
-    //Allows you to toggle the LEDs
+    //Posts the input to serial
     Button labelBtnLight(1, 86, 478, 60, "___________", Font16, LCD_COLOR_WHITE, 0xFF003538, 0xFF003538, 0xFF003538);
 
+    //Posts the input to serial
     Button labelBtnSound(1, 146, 478, 60, "___________", Font16, LCD_COLOR_WHITE, 0xFF003538, 0xFF003538, 0xFF003538);
 
     //Restart Button
@@ -192,58 +196,58 @@ void LoadRoomScreen()
 
     LoadBorder();
 
-    uint16_t Width = 75, Height = 50; // Default Width & Height defined for every button they're applied to
-    uint16_t X = 6, Y = 50; // Default X & Y Axis defined for every button they're applied to
-    uint16_t spaceX = 15, spaceY = 20; // Default X & Y Axis defined for every button they're applied to
+    uint16_t Width = 75, Height = 50; // Default Width & Height Setd for every button they're applied to
+    uint16_t X = 125, Y = 50; // Default X & Y Axis Setd for every button they're applied to
+    uint16_t spaceX = 15, spaceY = 20; // Default X & Y Axis Setd for every button they're applied to
 
     const string rooms[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
-    ButtonGroup RoomGroup(X, Y, Width, Height, 9, rooms, spaceX, spaceY, 2, -1, Font24, LCD_COLOR_RED, 0xFF003538, 0xFF003538);
+    ButtonGroup RoomGroup(X, Y, Width, Height, 9, rooms, spaceX, spaceY, 3, -1, Font24, LCD_COLOR_RED, 0xFF003538, 0xFF003538);
     while (1)
 	{
 		if (RoomGroup.Touched(0))
 		{
-			roomNumber = 1;                    //Define Room 1 as the selected room
+			roomNumber = 1;                    //Set Room 1 as the selected room
             LoadMainScreen();
         }
 		if (RoomGroup.Touched(1))
 		{
-			roomNumber = 2;                    //Define Room 2 as the selected room
+			roomNumber = 2;                    //Set Room 2 as the selected room
             LoadMainScreen();
 		}
 		if (RoomGroup.Touched(2))
 		{
-			roomNumber = 3;                    //Define Room 3 as the selected room
+			roomNumber = 3;                    //Set Room 3 as the selected room
             LoadMainScreen();
 		}
 		if (RoomGroup.Touched(3))
 		{
-			roomNumber = 4;                    //Define Room 4 as the selected room
+			roomNumber = 4;                    //Set Room 4 as the selected room
             LoadMainScreen();
 		}
 		if (RoomGroup.Touched(4))
 		{
-			roomNumber = 5;                    //Define Room 5 as the selected room
+			roomNumber = 5;                    //Set Room 5 as the selected room
             LoadMainScreen();
 		}
 		if (RoomGroup.Touched(5))
 		{
-			roomNumber = 6;                    //Define Room 6 as the selected room
+			roomNumber = 6;                    //Set Room 6 as the selected room
             LoadMainScreen();
 		}
 		if (RoomGroup.Touched(6))
 		{
-			roomNumber = 7;                    //Define Room 7 as the selected room
+			roomNumber = 7;                    //Set Room 7 as the selected room
             LoadMainScreen();
 		}
 		if (RoomGroup.Touched(7))
 		{
-			roomNumber = 8;                    //Define Room 8 as the selected room
+			roomNumber = 8;                    //Set Room 8 as the selected room
             LoadMainScreen();
 		}
 		if (RoomGroup.Touched(8))
 		{
-			roomNumber = 9;                    //Define Room 9 as the selected room
+			roomNumber = 9;                    //Set Room 9 as the selected room
             LoadMainScreen();
 		}
 
@@ -256,54 +260,54 @@ void LoadStartScreen()
 
     LoadBorder();
 
-    uint16_t Width = 75, Height = 50; // Default Width & Height defined for every button they're applied to
-    uint16_t X = 6, Y = 50;           // Default X & Y Axis defined for every button they're applied to
-    uint16_t spaceX = 15, spaceY = 20;           // Default X & Y Axis defined for every button they're applied to
+    uint16_t Width = 75, Height = 50; // Default Width & Height Setd for every button they're applied to
+    uint16_t X = 125, Y = 50; // Default X & Y Axis Setd for every button they're applied to
+    uint16_t spaceX = 15, spaceY = 20; // Default X & Y Axis Setd for every button they're applied to
 
     const string buildings[] = { "1", "2", "3", "4", "5", "6", "7", "8" };
 
-    ButtonGroup BuildingGroup(X, Y, Width, Height, 8, buildings, spaceX, spaceY, 2, -1, Font24, LCD_COLOR_RED, 0xFF003538, 0xFF003538);
+    ButtonGroup BuildingGroup(X, Y, Width, Height, 8, buildings, spaceX, spaceY, 3, -1, Font24, LCD_COLOR_RED, 0xFF003538, 0xFF003538);
 
     while (1)
 	{
 		if (BuildingGroup.Touched(0))
 		{
-			buildingNumber = 1;                //Define Building 1 as the selected building
+			buildingNumber = 1;                //Set Building 1 as the selected building
             LoadRoomScreen();
 		}
 		if (BuildingGroup.Touched(1))
 		{
-			buildingNumber = 2;                //Define Building 2 as the selected building
+			buildingNumber = 2;                //Set Building 2 as the selected building
             LoadRoomScreen();
 		}
 		if (BuildingGroup.Touched(2))
 		{
-			buildingNumber = 3;                //Define Building 3 as the selected building
+			buildingNumber = 3;                //Set Building 3 as the selected building
             LoadRoomScreen();
 		}
 		if (BuildingGroup.Touched(3))
 		{
-			buildingNumber = 4;                //Define Building 4 as the selected building
+			buildingNumber = 4;                //Set Building 4 as the selected building
             LoadRoomScreen();
 		}
 		if (BuildingGroup.Touched(4))
 		{
-			buildingNumber = 5;                //Define Building 5 as the selected building
+			buildingNumber = 5;                //Set Building 5 as the selected building
             LoadRoomScreen();
 		}
 		if (BuildingGroup.Touched(5))
 		{
-			buildingNumber = 6;                //Define Building 6 as the selected building
+			buildingNumber = 6;                //Set Building 6 as the selected building
             LoadRoomScreen();
 		}
 		if (BuildingGroup.Touched(6))
 		{
-			buildingNumber = 7;                //Define Building 7 as the selected building
+			buildingNumber = 7;                //Set Building 7 as the selected building
             LoadRoomScreen();
 		}
 		if (BuildingGroup.Touched(7))
 		{
-			buildingNumber = 8;                //Define Building 8 as the selected building
+			buildingNumber = 8;                //Set Building 8 as the selected building
             LoadRoomScreen();
 		}
 
